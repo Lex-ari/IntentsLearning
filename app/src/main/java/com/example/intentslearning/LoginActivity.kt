f873c8dcd05d5751779bbceb1e488cb67f801dc9
Lex_ari <amarianocello@gmail.com>
2020-11-17 00:50:02 -0800

    Finished Project

--- app/src/main/java/com/example/intentslearning/LoginActivity.kt
+++ app/src/main/java/com/example/intentslearning/LoginActivity.kt
@@ -3,12 +3,14 @@ package com.example.intentslearning
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     companion object {
         val EXTRA_USERNAME = "username"
+        val EXTRA_PASSWORD = "password"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +18,28 @@ class LoginActivity : AppCompatActivity() {
 
         button_login_signUp.setOnClickListener {
             val username = editText_login_username.text.toString()
-            val registrationIntent = Intent(this, RegistrationActivity::class.java)
-            registrationIntent.putExtra(EXTRA_USERNAME, username)
-            startActivity(registrationIntent) // SEEEEEEEEEEENNNNNNND IIIIIIIIIIIITT
+            val password = editText_login_password.text.toString()
+            val registrationIntent = Intent(this, RegistrationActivity::class.java).apply{
+                putExtra(EXTRA_USERNAME, username)
+                putExtra(EXTRA_PASSWORD, password)
+            }
+            //startActivity(registrationIntent) // SEEEEEEEEEEENNNNNNND IIIIIIIIIIIITT
+            startActivityForResult(registrationIntent, 1)
 
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                editText_login_username.setText(data?.getStringExtra("newusername"))
+                editText_login_password.setText(data?.getStringExtra("newpassword"))
+            }
+            if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Registration Cancelled", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/intentslearning/RegistrationActivity.kt
+++ app/src/main/java/com/example/intentslearning/RegistrationActivity.kt
@@ -1,15 +1,48 @@
 package com.example.intentslearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
         val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME)
-        Toast.makeText(this, username, Toast.LENGTH_SHORT).show()
+        val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD)
+
+        editText_registration_newUsername.setText(username)
+        editText_registration_password.setText(password)
+
+        button_registration_register.setOnClickListener {// Checks if all the fields are filled in and correct
+            if (editText_registration_name.text.toString() == ""){
+                Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show()
+            } else if(editText_registration_newUsername.text.toString() == ""){
+                Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show()
+            } else if(editText_registration_password.text.toString() == ""){
+                Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+            } else if(editText_registration_confirmPassword.text.toString() != editText_registration_password.text.toString()){
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            } else if(editText_registration_email.text.toString() == ""){
+                Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
+            } else { // Start "mailing" back all the answers :D
+                val loginIntent = Intent(this, LoginActivity::class.java).apply{
+                    putExtra("newusername", editText_registration_newUsername.text.toString())
+                    putExtra("newpassword", editText_registration_password.text.toString())
+                    // Note to future developer self: Make sure that the variable agrees with whatever is trying to get data from. Otherwise it's like trying to find ice cubes in the dishwasher. Doesn't make sense.
+                }
+                setResult(RESULT_OK, loginIntent)
+                finish()
+            }
+        }
+
+        button_registration_cancelRegistration.setOnClickListener{
+            setResult(RESULT_CANCELED)
+            finish()
+        }
     }
 }
